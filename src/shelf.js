76dfda92a04f5fd65bf083941bc662a27c6505c3
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-03-26 23:01:46 -0400

    Add shelfbook function to shelf file

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,22 @@
 
+function shelfBook(book, shelf) {
+if (shelf.length < 3) {
+  shelf.unshift(book);
+}
+return shelf
+}
+
+function unshelfBook(bookTitle, shelf) {
+if (book === bookTitle) {
+  shelf.splice(bookTitle)
+}
+console.log(shelf)
+return shelf
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
